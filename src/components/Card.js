afdf4dece3fdd1823b8516a230a9aa9ffe77efde
Vergel Mañalac <verge2015@gmail.com>
2022-07-29 16:49:13 +0800

    Delete unused assets
    Delete unused assets and remove some comments

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,9 @@
-// import * as React from 'react';
-// import Card from '@mui/material/Card';
-// import CardActions from '@mui/material/CardActions';
-// import CardContent from '@mui/material/CardContent';
-// import CardMedia from '@mui/material/CardMedia';
-// import Button from '@mui/material/Button';
-// import Typography from '@mui/material/Typography';
-// import { Grid } from '@mui/material';
-
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import ButtonBase from '@mui/material/ButtonBase';
-// import { motion } from "framer-motion";
 
 const Img = styled('img')({
     margin: 'auto',
@@ -23,12 +13,8 @@ const Img = styled('img')({
   });
 
 const MediaCard = (props) => {
-
     const { title, description, icon } = props;
-
     return (
-
-
         <Paper
         sx={{
           p: 2,
@@ -38,7 +24,6 @@ const MediaCard = (props) => {
           maxWidth: '50%',
           flexGrow: 1,
           opacity: 0.9,
-        //   backgroundColor: 'transparent'
           backgroundColor: (theme) =>
             theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
         }}
@@ -54,21 +39,8 @@ const MediaCard = (props) => {
                 <Typography variant="body1" gutterBottom  color='#1C205F' align='justify'>
                     {description}
                 </Typography>
-                {/* <Typography variant="body2" color="text.secondary">
-                  ID: 1030114
-                </Typography> */}
               </Grid>
-              {/* <Grid item>
-                <Typography sx={{ cursor: 'pointer' }} variant="body2">
-                  Remove
-                </Typography>
-              </Grid> */}
             </Grid>
-            {/* <Grid item>
-              <Typography variant="subtitle1" component="div">
-                $19.00
-              </Typography>
-            </Grid> */}
           </Grid>
 
           <Grid item>
@@ -78,32 +50,6 @@ const MediaCard = (props) => {
           </Grid>
         </Grid>
       </Paper>
-
-        // <Grid container spacing={3} style={{ marginLeft: '1%', marginRight: '1%' }} >
-        // <Grid item xs={12}>
-
-        // <Card sx={{ maxWidth: '80%', backgroundColor: 'transparent', color: '#ffffff' }}>
-        //   <CardMedia
-        //     component="img"
-        //     height="140"
-        //     // image="/static/images/cards/contemplative-reptile.jpg"
-        //     // alt="green iguana"
-        //   />
-        //   <CardContent>
-        //     <Typography gutterBottom variant="h5" component="div"  color='#4D9AE4'>
-        //     {title}
-        //     </Typography>
-        //     <Typography variant="body3" color='#1C205F'>
-        //       {description}
-        //     </Typography>
-        //   </CardContent>
-        //   {/* <CardActions>
-        //     <Button size="small">Share</Button>
-        //     <Button size="small">Learn More</Button>
-        //   </CardActions> */}
-        // </Card>
-        // </Grid>
-        // </Grid>
       );
 }
 
